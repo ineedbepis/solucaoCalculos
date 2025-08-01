aaad3ec34e321d9063b440bd299a0902a78ddbb0
LabInfo <LabInfo@TERREOA2M20>
2025-07-31 22:20:59 -0300

    Adicionar arquivos de projeto.

---
+++ Projeto_Calculos/Calculos.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace Projeto_Calculos
+{
+    partial class Calculos
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtN1 = new System.Windows.Forms.TextBox();
+            this.txtN2 = new System.Windows.Forms.TextBox();
+            this.lblSinal = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.btnSomar = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.btnFechar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtN1
+            // 
+            this.txtN1.Location = new System.Drawing.Point(315, 69);
+            this.txtN1.Name = "txtN1";
+            this.txtN1.Size = new System.Drawing.Size(100, 26);
+            this.txtN1.TabIndex = 0;
+            this.txtN1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtN2
+            // 
+            this.txtN2.Location = new System.Drawing.Point(315, 136);
+            this.txtN2.Name = "txtN2";
+            this.txtN2.Size = new System.Drawing.Size(100, 26);
+            this.txtN2.TabIndex = 1;
+            this.txtN2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lblSinal
+            // 
+            this.lblSinal.AutoSize = true;
+            this.lblSinal.BackColor = System.Drawing.SystemColors.Window;
+            this.lblSinal.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblSinal.Location = new System.Drawing.Point(271, 136);
+            this.lblSinal.Name = "lblSinal";
+            this.lblSinal.Size = new System.Drawing.Size(20, 22);
+            this.lblSinal.TabIndex = 2;
+            this.lblSinal.Text = "?";
+            this.lblSinal.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Location = new System.Drawing.Point(286, 185);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(162, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "_________________";
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblResultado.Location = new System.Drawing.Point(290, 222);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(158, 23);
+            this.lblResultado.TabIndex = 4;
+            this.lblResultado.Text = "?";
+            this.lblResultado.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblResultado.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // btnSomar
+            // 
+            this.btnSomar.Location = new System.Drawing.Point(124, 273);
+            this.btnSomar.Name = "btnSomar";
+            this.btnSomar.Size = new System.Drawing.Size(75, 32);
+            this.btnSomar.TabIndex = 5;
+            this.btnSomar.Text = "&Somar";
+            this.btnSomar.UseVisualStyleBackColor = true;
+            this.btnSomar.Click += new System.EventHandler(this.btnSomar_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(328, 273);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(75, 32);
+            this.btnLimpar.TabIndex = 6;
+            this.btnLimpar.Text = "&Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnFechar
+            // 
+            this.btnFechar.Location = new System.Drawing.Point(528, 273);
+            this.btnFechar.Name = "btnFechar";
+            this.btnFechar.Size = new System.Drawing.Size(75, 32);
+            this.btnFechar.TabIndex = 7;
+            this.btnFechar.Text = "$Fechar";
+            this.btnFechar.UseVisualStyleBackColor = true;
+            this.btnFechar.Click += new System.EventHandler(this.btnFechar_Click);
+            // 
+            // Calculos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.ClientSize = new System.Drawing.Size(722, 531);
+            this.Controls.Add(this.btnFechar);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnSomar);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblSinal);
+            this.Controls.Add(this.txtN2);
+            this.Controls.Add(this.txtN1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "Calculos";
+            this.Text = "Cálculos com Botões";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtN1;
+        private System.Windows.Forms.TextBox txtN2;
+        private System.Windows.Forms.Label lblSinal;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Button btnSomar;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button btnFechar;
+    }
+}
+
---
+++ Projeto_Calculos/Calculos.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projeto_Calculos
+{
+    public partial class Calculos : Form
+    {
+        public Calculos()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            txtN1.Clear();
+            txtN2.Clear();
+            lblSinal.Text = "?";
+            lblResultado.Text = "?";
+            txtN1.Focus();
+        }
+
+        private void btnSomar_Click(object sender, EventArgs e)
+        {
+            double  a, b;
+
+            try
+            {
+                lblSinal.Text = "+";
+                a = double.Parse(txtN1.Text);
+                b = double.Parse(txtN2.Text);
+                lblResultado.Text = (a + b).ToString();
+            } 
+            catch (FormatException)
+            {
+                MessageBox.Show("Preencha corretamente os números!");
+                lblSinal.Text = "?";
+            }
+            
+        }
+
+        private void btnFechar_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
